f459a2bf819750ae522e3ec2fd97d14db204254a
vantouse <ivcos2003@gmail.com>
2023-11-11 15:07:50 +0500

    Task 2: added new rules to the game.

--- cv/cv_task2_main.py
+++ cv/cv_task2_main.py
@@ -31,14 +31,30 @@ fingers_lr = [{4: (0, 0, 50), 8: (0, 0, 100), 12: (0, 0, 150), 16: (0, 0, 200), 
               {4: (0, 50, 0), 8: (0, 100, 0), 12: (0, 150, 0), 16: (0, 200, 0), 20: (0, 255, 0)}]
 bubbles = list()
 
+fps = 0
 time_init = time.time_ns() // (10 ** 9)
 time_prev = time.time_ns() // (10 ** 9)
-fps = 0
+time_interval = 2
+
+time_game_res = time.time_ns() // (10 ** 9)
+game_res = None
 
 while capture_cam.isOpened():
-    # timer
+    # timer and game state tracking mechanism
     time_curr = time.time_ns() // (10 ** 9)
-    if time_curr - time_prev >= 2:
+    if not game_res and len(bubbles) == 0 and time_curr - time_init >= 2 > time_curr - time_prev:
+        # winning conditions
+        game_res = 'YOU WIN!'
+        time_game_res = time_curr
+    if not game_res and len(bubbles) >= 10:
+        # losing conditions
+        game_res = 'YOU LOSE!'
+        time_game_res = time_curr
+    if game_res and time_curr - time_game_res > 3:
+        # wait 3 sec after win/loss
+        break
+
+    if time_curr - time_prev >= time_interval and not game_res:
         # add new bubbles to frame in random positions
         bubble = Bubble((random.randrange(default_res[0]), random.randrange(default_res[1])),
                         round(frame_h / 20),
@@ -63,7 +79,7 @@ while capture_cam.isOpened():
     results = hands.process(cam_frame)
     cam_frame = cv2.cvtColor(cam_frame, cv2.COLOR_RGB2BGR)
 
-    # game core: create bubbles in random positions and bust them with fingertips
+    # create bubbles in random positions and bust them with fingertips
     if results.left_hand_landmarks:
         for landmark_num in range(len(results.left_hand_landmarks.landmark)):
             landmark = results.left_hand_landmarks.landmark[landmark_num]
@@ -92,9 +108,15 @@ while capture_cam.isOpened():
                 if bubble.busted_with_finger(1, fingertip, results.right_hand_landmarks.landmark[fingertip]):
                     bubbles.remove(bubble)
 
-    #draw bubbles
+    # draw bubbles
     for bubble in bubbles:
         cv2.circle(cam_frame, bubble.pos, bubble.size, fingers_lr[bubble.hand_type][bubble.finger_type], -1)
+
+    # print game result
+    if game_res:
+        game_res_txt_color = (0, 255, 0) if game_res == 'YOU WIN!' else (0, 0, 255)
+        cv2.putText(cam_frame, game_res, (round(frame_w / 2) - 182, round(frame_h / 2) + 25), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 0), 16, cv2.LINE_AA)
+        cv2.putText(cam_frame, game_res, (round(frame_w / 2) - 182, round(frame_h / 2) + 25), cv2.FONT_HERSHEY_COMPLEX, 2, game_res_txt_color, 8, cv2.LINE_AA)
 
     # camera preview
     cv2.imshow('cam_frame', cam_frame)
